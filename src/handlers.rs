3d08c7f5571b5c2d1b3c285fdf527a57c2ec87b5
Arnav Kohli <arnavkohli2003@gmail.com>
2023-11-25 00:35:19 +0000

    Added routes for event
    Added routes in handlers.rs for event api call.
    
    Signed-off-by: Arnav Kohli <arnavkohli2003@gmail.com>

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,3 +1,19 @@
-pub async fn base() -> &'static str {
-    "Welcome to ScheduLink!"
-}
+    pub async fn base() -> &'static str {
+        "Handling GET request for /"
+    }
+
+    pub async fn event_get() -> &'static str {
+        "Handling GET request for /api/event"
+    }
+
+    pub async fn event_post() -> &'static str {
+        "Handling POST request for /api/event"
+    }
+
+    pub async fn event_put() -> &'static str {
+        "Handling PUT request for /api/event"
+    }
+
+    pub async fn event_delete() -> &'static str {
+        "Handling DELETE request for /api/event"
+    }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 mod handlers;
-use axum::{Router, routing::get};
+use axum::{Router, routing::get, routing::post, routing::put, routing::delete};
 
 #[tokio::main]
 async fn main() {
     // Dynamic port allocation for fl{/}
-    let port = std::env::var("PORT").unwrap_or_else(|_| "3000".to_string());
+    let port = std::env::var("PORT").unwrap_or_else(|_| "8080".to_string());
     let addr = format!("0.0.0.0:{}", port);
 
     // Routes defined for the app
     let app =
         Router::new()
-            .route("/", get(handlers::base));
+            .route("/", get(handlers::base))
+            .route("/api/event", get(handlers::event_get))
+            .route("/api/event", post(handlers::event_post))
+            .route("/api/event", put(handlers::event_put))
+            .route("/api/event", delete(handlers::event_delete));
 
-    // Server the app asynchronously
+    // Serve the app asynchronously
     axum::Server::bind(&addr.parse().unwrap())
         .serve(app.into_make_service())
         .await
